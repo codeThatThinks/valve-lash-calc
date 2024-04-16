293bfcc2525609ddda94c5e2a5e9c13a871526d9
Ian Glen <ian@ianglen.me>
2024-04-16 01:45:57 -0400

    Fix uniqueness bug; pre-sort by number of choices; other refactoring

--- calc_lash.py
+++ calc_lash.py
@@ -1,11 +1,10 @@
 """Ford Valve Lash Bucket Calculator"""
 
-import sys
 import time
 
-# Engine Parameters
-CYLINDERS = 4
-VALVES = 16
+
+#### CONSTANTS ####
+NUM_VALVES = 16
 
 # Lash Limits
 INTAKE_MIN =        0.007   # in
@@ -29,15 +28,24 @@ BUCKET_INV = [302, 322, 342, 342, 342, 342, 362, 362, 362, 362, 362, 362, 362, 3
 BUCKET_INV += CURRENT_BUCKETS
 BUCKET_INV.sort()
 
-ideal_buckets = []
-possible_buckets = []
+# Bucket Choices for each valve
+# Note: each choice must be universally unique, so the index for
+# the bucket in BUCKET_INV is used instead of the actual bucket ID
+CHOICES = []
 """
 [
-    [(bucket, weight), ...],
+    (
+        <valve index>,
+        <number of choices>,
+        [<list of choices>],
+        [<list of weights>]
+    ),
     ...
 ]
 """
 
+
+#### HELPER FUNCTIONS ####
 
 def bucket_to_in(bucket_id:int) -> float:
     """
@@ -53,6 +61,10 @@ def bucket_to_in(bucket_id:int) -> float:
 def select_closest_bucket(buckets:list[int], thickness_in:float, method:str="round") -> int:
     """
     Select bucket in list closest to a thickness in inches
+
+    method =    round: round to nearest; round up if exactly halfway
+                floor: always round down
+                ceil: always round up
     """
 
     ideal_bucket = int(round((thickness_in * 25.4 - 3) * 1000))
@@ -70,150 +82,162 @@ def select_closest_bucket(buckets:list[int], thickness_in:float, method:str="rou
         if b > ideal_bucket:
             if method == "round":
                 return b if b - ideal_bucket <= ideal_bucket - buckets[i - 1] else buckets[i - 1]
-            elif method == "ceil":
+
+            if method == "ceil":
                 return b
-            elif method == "floor":
+
+            if method == "floor":
                 return buckets[i - 1]
-            else:
-                raise ValueError("Invalid method")
+
+            raise ValueError("Invalid method")
+
+    # should never end up here
+    raise RuntimeError("Could not find closest bucket")
 
 
-def select_possible_buckets(buckets:list[int], thickness_min_in:float, thickness_max_in:float) -> list[int]:
+def select_possible_buckets_from_inventory(thickness_min_in:float, thickness_max_in:float) -> list[int]:
     """
-    Select buckets in list that fall within thickness range in inches
+    Select buckets in inventory that fall within thickness range in inches (inclusive)
+    Returns a list of bucket indicies in BUCKET_INV
     """
 
-    if thickness_min_in > bucket_to_in(max(buckets)) or thickness_max_in < bucket_to_in(min(buckets)):
+    if thickness_min_in > bucket_to_in(max(BUCKET_INV)) or thickness_max_in < bucket_to_in(min(BUCKET_INV)):
         return []
 
-    min_bucket = select_closest_bucket(buckets, thickness_min_in, method="ceil")
-    max_bucket = select_closest_bucket(buckets, thickness_max_in, method="floor")
+    min_bucket = select_closest_bucket(BUCKET_INV, thickness_min_in, method="ceil")
+    max_bucket = select_closest_bucket(BUCKET_INV, thickness_max_in, method="floor")
 
-    i1 = buckets.index(min_bucket)
+    i1 = BUCKET_INV.index(min_bucket)
 
-    for i2 in range(len(buckets) - 1, -1, -1):
-        if buckets[i2] == max_bucket:
+    for i2 in range(len(BUCKET_INV) - 1, -1, -1):
+        if BUCKET_INV[i2] == max_bucket:
             break
 
-    return buckets[i1:i2+1]
+    return list(range(i1, i2 + 1))
 
 
+def generate_choices(valve_index:int, bucket:int, lash:float, lash_min:float, lash_max:float, lash_target:float):
+    """
+    Generate possible bucket choices for a valve and add to CHOICES list
+    Also print out valve data
+    """
+
+    # calculate valve distance, ideal bucket, and possible bucket choices
+    valve_dist = bucket_to_in(bucket) + lash
+    ideal_bucket = select_closest_bucket(BUCKETS, valve_dist - lash_target)
+    possible = select_possible_buckets_from_inventory(valve_dist - lash_max, valve_dist - lash_min)
+    possible_with_w = [(c, int(round(abs(valve_dist - bucket_to_in(c) - lash_target), 4) * 10000)) for c in possible]
+
+    # sort possible choices from lowest to highest weight
+    possible_with_w.sort(key=lambda p: p[1])
+
+    # add no choice (purchase) option
+    possible_with_w.append((None, 10000))
+
+    # split apart into possible choices and weights (deviations)
+    possible_choices = [p[0] for p in possible_with_w]
+    possible_weights = [p[1] for p in possible_with_w]
+
+    # print out valve data
+    print(f'#{valve_index}\tIdeal: {ideal_bucket}')
+    print(f'\t{possible_choices}')
+    print(f'\t{possible_weights}')
+    print("")
+
+    # sanity check
+    assert len(possible_choices) == len(possible_weights)
+
+    # add to choices list
+    CHOICES.append((valve_index, len(possible_choices), possible_choices, possible_weights))
+
+
+#### OPTIMIZATION FUNCTION ####
+
+# (global variable) best combo seen so far
 best_weight = None
-def iterate_choices_best(pos, current_choices, current_weight, combos):
-    """Attempt #2 -- skip checking some branches by keeping track of the best total weight so far"""
+best_choices = None
 
-    global best_weight, possible_buckets
+def iterate_choices(pos, current_weight, *current_choices):
+    """
+    pos: Current index in CHOICES
+    current_weight: Total weight of choices made so far
+    current_choices: List of choices made so far (as vargs for speed)
+    """
 
-    if pos >= len(possible_buckets):
-        # no more choices to make, add this combo to list
+    global best_weight, best_choices
 
-        combos.append((current_weight, current_choices))
-
-        # record weight if it's the best we've seen so far
+    if pos == NUM_VALVES:
+        # no more choices to make, check this combo
+        # if it's the best we've seen so far, update best_weight and print it out
         if best_weight is None or current_weight < best_weight:
             best_weight = current_weight
-            print(current_weight, current_choices)
+            best_choices = current_choices
+            print(f'w = {current_weight}\tc = {current_choices}')
 
         return
 
-    # iterate through choices for current position
-    for i in range(len(possible_buckets[pos])):
-        c = possible_buckets[pos][i][0]
-        w = possible_buckets[pos][i][1]
+    # recurse through choices for current position
+    for i in range(CHOICES[pos][1]):
+        c = CHOICES[pos][2][i]
+        w = CHOICES[pos][3][i]
 
         if best_weight is not None and current_weight + w >= best_weight:
-            # we're already worst than other combo, so skip this branch
+            # we're not any better than prev. combo, so skip this branch
             continue
 
         if c is not None and c in current_choices:
             # choice already selected, skip
             continue
 
-        iterate_choices_best(pos + 1, current_choices + [c], current_weight + w, combos)
+        iterate_choices(pos + 1, current_weight + w, *current_choices, c)
 
+
+#### SCRIPT ENTRYPOINT ####
 
 if __name__ == "__main__":
-    print(f'Intake target: {INTAKE_TARGET:.04f} in')
-    print(f'Intake range: {INTAKE_MIN:.04f} - {INTAKE_MAX:.04f} in')
-    print(f'Exhaust target: {EXHAUST_TARGET:.04f} in')
-    print(f'Exhaust range: {EXHAUST_MIN:.04f} - {EXHAUST_MAX:.04f} in')
+    print("Ford Duratec/Ecoboost Valve Lash Calculator")
+    print("")
+    print(f'Intake lash: range {INTAKE_MIN:.04f} - {INTAKE_MAX:.04f} in, target {INTAKE_TARGET:.04f} in')
+    print(f'Exhaust lash: range {EXHAUST_MIN:.04f} - {EXHAUST_MAX:.04f} in, target {EXHAUST_TARGET:.04f} in')
     print("")
 
-    # for intake valves, calculate ideal bucket and possible buckets in inventory
-    print("Intake:")
+    # generate possible choices for each valve and print it out
+    print("Intake Valves:")
     i = 0
-    for bucket, lash in zip(CURRENT_BUCKETS[:VALVES // 2], CURRENT_LASH[:VALVES // 2]):
-        valve_dist = bucket_to_in(bucket) + lash
-        ideal_bucket = select_closest_bucket(BUCKETS, valve_dist - INTAKE_TARGET)
-        possible = select_possible_buckets(BUCKET_INV, valve_dist - INTAKE_MAX, valve_dist - INTAKE_MIN)
-        possible_with_dev = [(c, int(round(abs(valve_dist - bucket_to_in(c) - INTAKE_TARGET)*100000))) for c in possible]
-
-        # sort possible by deviation
-        possible_with_dev.sort(key=lambda c: c[1])
-
-        #possible_uniq = list(set(possible))
-        #possible_uniq.sort()
-        print(f'#{i+1}\tValve Distance: {valve_dist:.04f} in')
-        print(f'\tIdeal: {ideal_bucket}')
-        #print(f"\tPossible: {possible_uniq}")
-
-        # add no choice (purchase) option
-        possible_with_dev.append((None, 10000))
-
-        ideal_buckets.append(ideal_bucket)
-        possible_buckets.append(possible_with_dev)
-
+    for bucket, lash in zip(CURRENT_BUCKETS[:NUM_VALVES // 2], CURRENT_LASH[:NUM_VALVES // 2]):
+        generate_choices(i, bucket, lash, INTAKE_MIN, INTAKE_MAX, INTAKE_TARGET)
         i += 1
-        print("")
-
     print("")
 
-    # for exhaust valves, calculate ideal bucket and possible buckets in inventory
-    print("Exhaust:")
+    print("Exhaust Valves:")
     i = 0
-    for bucket, lash in zip(CURRENT_BUCKETS[VALVES // 2:], CURRENT_LASH[VALVES // 2:]):
-        valve_dist = bucket_to_in(bucket) + lash
-        ideal_bucket = select_closest_bucket(BUCKETS, valve_dist - EXHAUST_TARGET)
-        possible = select_possible_buckets(BUCKET_INV, valve_dist - EXHAUST_MAX, valve_dist - EXHAUST_MIN)
-        possible_with_dev = [(c, int(round(abs(valve_dist - bucket_to_in(c) - INTAKE_TARGET)*100000))) for c in possible]
-        
-        # sort possible by deviation
-        possible_with_dev.sort(key=lambda c: c[1])
-
-        #possible_uniq = list(set(possible))
-        #possible_uniq.sort()
-        print(f'#{i+1}\tValve Distance: {valve_dist:.04f} in')
-        print(f'\tIdeal: {ideal_bucket}')
-        #print(f"\tPossible: {possible_uniq}")
-
-        # add no choice (purchase) option
-        possible_with_dev.append((None, 10000))
-
-        ideal_buckets.append(ideal_bucket)
-        possible_buckets.append(possible_with_dev)
-
+    for bucket, lash in zip(CURRENT_BUCKETS[NUM_VALVES // 2:], CURRENT_LASH[NUM_VALVES // 2:]):
+        generate_choices(i, bucket, lash, EXHAUST_MIN, EXHAUST_MAX, EXHAUST_TARGET)
         i += 1
-        print("")
+    print("")
 
-    print("Possible buckets = [")
-    for b in possible_buckets:
-        print(f'\t{[c[0] for c in b]}')
-    print("]\n")
+    # sanity check
+    assert len(CHOICES) == NUM_VALVES
 
-    print("Possible deviations = [")
-    for b in possible_buckets:
-        print(f'\t{[c[1] for c in b]}')
-    print("]\n")
+    # sort CHOICES by valves with fewest choices to most
+    CHOICES.sort(key=lambda v: v[1])
 
-    combos = []
+    # run optimization function to iterate through choices
+    print("Calculating best combos...")
     s = time.perf_counter()
-    iterate_choices_best(0, [], 0, combos)
+    iterate_choices(0, 0)
     e = time.perf_counter()
 
     print("")
     print(f'Iteration took {e - s:.06f} sec')
 
-    # print("Best Combos:")
-    # for i in range(len(combos)):
-    #     print(f'{combos[i][0]}\t{combos[i][1]}')
-    # print("")
+    # map best choices back to valve index and bucket ID, and reorder by valve index
+    best_choices_mapped = [(CHOICES[i][0], BUCKET_INV[j]) for i, j in enumerate(best_choices)]
+    best_choices_mapped.sort(key=lambda v: v[0])
+
+    # print out the best combo
+    print("Best combo:")
+    print(f'\tWeight: {best_weight:.04f}')
+    for v in best_choices_mapped:
+        print(f'\t#{v[0]}\t{v[1]}')
+    print("")
